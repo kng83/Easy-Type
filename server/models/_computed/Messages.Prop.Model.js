c2240e536712b2411a4bd59e519c99249ee08972
kng83 <pawel.kng@gmail.com>
2019-03-02 19:40:14 +0100

    some

--- server/models/_computed/Messages.Prop.Model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-//#151
-var MessagesPropModel = /** @class */ (function () {
-    function MessagesPropModel() {
-    }
-    return MessagesPropModel;
-}());
-exports.MessagesPropModel = MessagesPropModel;
--- server/models/_computed/User.Prop.Model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-//#151
-var UserPropModel = /** @class */ (function () {
-    function UserPropModel() {
-    }
-    return UserPropModel;
-}());
-exports.UserPropModel = UserPropModel;
