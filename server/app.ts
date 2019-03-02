c8e28abdc9762ad26c8c14476b79d4cb4d1b286c
kng83 <pawel.kng@gmail.com>
2019-03-02 23:22:15 +0100

    some

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,31 @@ import txt  from './Text_Files/alpha.txt';
 import content from './Gql_Files/first.gql';
 import {ApolloServer, gql} from 'apollo-server';
 
+import mssql from 'mssql'
+
+
+let sql = mssql as any;
+
+(async () => {
+    try {
+        await sql.connect('mssql://sa:12345@localhost:49714/Pyszczek'); //
+        const result = await sql.query(
+               `SELECT TOP 5
+                [Name],[Age]
+                FROM [Pyszczek].[dbo].[First]`);
+        console.log(result.recordset);
+
+        const second = await sql.query(
+            `INSERT INTO [Pyszczek].[dbo].[First]
+             VALUES ('Puszek',1);`//
+        )
+
+    } catch (err) {
+        // ... error checks
+        console.log(err.message);//
+    }
+})();
+
 const some ='some'
 
 const typeDefs = gql(content);
