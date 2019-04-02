fcc399c5956a4139a1a674fc6fd03b844c64b7ca
kng83 <pawel.kng@gmail.com>
2019-04-02 19:01:10 +0200

    some

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,23 @@
-const uWS = require('uWebSockets.js');
+
+import app from 'uWebSockets.js'
 const port = 9001;
 
-const app = uWS.SSLApp({
+const apps = app.SSLApp({
   key_file_name: 'misc/39321665_localhost.key',
   cert_file_name: 'misc/39321665_localhost.cert',
   passphrase: '1234'
+
 }).any('/anything', (res, req) => {
-  res.end('Any route with method: ' + req.getMethod());``
+  console.log('jest tu');
+  res.end('Any route with method: ' + req.getMethod());
+
 }).get('/user/agent', (res, req) => {
   /* Read headers */
+  res.writeHeader('uWebSockets', 'kot');
+
   res.end('Your user agent is: ' + req.getHeader('user-agent') + ' thank you, come again!');
 }).get('/static/yes', (res, req) => {
+  
   /* Static match */
   res.end('This is very static');
 }).get('/candy/:kind', (res, req) => {
@@ -18,8 +25,10 @@ const app = uWS.SSLApp({
   res.end('So you want candy? Have some ' + req.getParameter(0) + '!');
 }).get('/*', (res, req) => {
   /* Wildcards - make sure to catch them last *///
-  res.end('HelloWord');
+  
+  res.end('./index.html');
 }).listen(port, (token) => {
+
   if (token) {
     console.log('Listening to port ' + port);
   } else {
