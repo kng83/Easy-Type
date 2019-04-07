4e29b1b4b61573d348bd85d01892600c14178627
kng83 <pawel.kng@gmail.com>
2019-04-07 18:56:07 +0200

    some

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import {Response} from 'express';
 import txt  from './Text_Files/alpha.txt';
 import {ctrl} from './Controllers/first.ctrl'
 import bodyParser from 'body-parser';
@@ -8,8 +9,13 @@ const app = express();
 const port = 3000;
 console.log(txt);
 
+
 app.use(bodyParser.json({type:'application/*+json'}))
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res:Response) =>{
+    //res.setHeader('X-Powered-By', null);
+    res.removeHeader('X-Powered-By')
+ res.send('Hello World!')
+});
 app.get('/getAll',ctrl.getAll)
 app.get('/getOldest',ctrl.getOldest);
 app.get('/getById/:id',ctrl.getById)
