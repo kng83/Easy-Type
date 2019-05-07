96c02ae3c64050df3f91db8f1acadef60deabd50
kng83 <pawel.kng@gmail.com>
2019-05-08 00:20:45 +0200

    some

--- server/app.ts
+++ server/app.ts
@@ -13,17 +13,28 @@ app.get('/', (req, res) => res.send('Hello World!'))//
 const wss = new WebSocket.Server({
   port: 3001,
   verifyClient: (client) => {
-    const {host} = client.req.headers; 
+    const { host } = client.req.headers;
     console.log(host);
     return true
   }
 }).on('connection', (ws) => {
   ws.on('message', (message) => {
-    mainController(message).then(msg=>{  
+    mainController(message).then(msg => {
       console.log(msg);
+     console.log(useOfMemory());
       ws.send(msg)
-    }).catch(err=>console.log(err))
+    }).catch(err => console.log(err))
   });
+}).on('close', (ws) => {
+  console.log('close connection');
+  ws.send('close')
 })
-
+//console.log(process.memoryUsage());
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+function useOfMemory() {
+  const used = process.memoryUsage();
+  for (let key in used) {
+    console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+  }
+}
